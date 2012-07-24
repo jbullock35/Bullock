99f230369fbf46aaf6a457dcd5d00f1f20a3f095
jbullock <jbullock@92c5aa16-fb2c-7740-8e8b-229359ae7b01>
2012-07-24 21:13:02 +0000

    Bullock package: added push, pop, shift, unshift.

---
+++ methods/R_utils/trunk/package_sources/Bullock/R/stackUtils.R
@@ -0,0 +1,39 @@
+# stackUtils.R
+# created 2012 July 24
+
+# Provides Perl-like push(), pop(), shift(), and unshift().  Adapted from
+# Jeffrey A. Ryan's code at 
+# http://www.lemnica.com/esotericR/Introducing-Closures/.
+
+new_stack <- function() { 
+  stack <- new.env()
+  stack$.Data <- vector()
+  stack$push <- function(x) .Data <<- c(.Data,x)
+  stack$pop  <- function() {
+    tmp <- .Data[length(.Data)]
+    .Data <<- .Data[-length(.Data)]
+    return(tmp)
+  }
+  stack$shift   <- function(x) .Data <<- c(x, .Data)
+  stack$unshift <- function() {
+    tmp <- .Data[1]
+    .Data <<- .Data[-1]
+    return(tmp)
+  }
+  environment(stack$push)    <- as.environment(stack)
+  environment(stack$pop)     <- as.environment(stack)
+  environment(stack$shift)   <- as.environment(stack)
+  environment(stack$unshift) <- as.environment(stack)
+  
+  class(stack) <- "stack"
+  stack
+}
+    
+push    <- function(x, value, ...) UseMethod("push")     # add to end of vector
+pop     <- function(x, ...)        UseMethod("pop")      # return last value from vector and remove it from vector
+shift   <- function(x, value, ...) UseMethod("shift")
+unshift <- function(x, ...)        UseMethod("unshift")
+push.stack    <- function(x, value, ...) x$push(value)
+pop.stack     <- function(x)             x$pop()
+shift.stack   <- function(x, value, ...) x$shift(value)
+unshift.stack <- function(x)             x$unshift()
