d766ddb84d31c8d6ddbdf596711fafd600c4baa1
jbullock <jbullock@92c5aa16-fb2c-7740-8e8b-229359ae7b01>
2014-05-24 00:53:54 +0000

    Incremented version.  The only change is to the default of the "newrange" argument in rescale().

--- methods/R_utils/trunk/package_sources/Bullock/R/rescale.R
+++ methods/R_utils/trunk/package_sources/Bullock/R/rescale.R
@@ -1,16 +1,17 @@
-rescale<-function(x,newrange) {
+rescale <- function(x, newrange = c(0, 1)) {
  if(nargs() > 1 && is.numeric(x) && is.numeric(newrange)) {
-  # if newrange has max first, reverse it
+
+   # if newrange has max first, reverse it
   if(newrange[1] > newrange[2]) {
-   newmin<-newrange[2]
-   newrange[2]<-newrange[1]
-   newrange[1]<-newmin
+    newmin <- newrange[2]
+    newrange[2] <- newrange[1]
+    newrange[1] <- newmin
   }
-  #xrange<-range(x)
-  xrange<-range(x,na.rm=TRUE)
+
+  xrange <- range(x, na.rm = TRUE)
   if(xrange[1] == xrange[2]) stop("can't rescale a constant vector!")
-  mfac<-(newrange[2]-newrange[1])/(xrange[2]-xrange[1])
-  invisible(newrange[1]+(x-xrange[1])*mfac)
+  mfac <- (newrange[2] - newrange[1]) / (xrange[2] - xrange[1])
+  invisible(newrange[1] + (x-xrange[1])*mfac)
  }
  else {
   cat("Usage: rescale(x,newrange)\n")
