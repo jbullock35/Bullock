4b915f6632ecdb9b279af54ad8e2dd24f52288b9
John Bullock <john@johnbullock.org>
2019-12-25 10:26:44 -0600

    Added documentation page for the package.

---
+++ R/Bullock-package.R
@@ -0,0 +1,16 @@
+#' Bullock: convenience functions and tools for table generation
+#' 
+#' This package provides tools that I use in my code. Most of the 
+#' tools are simple convenience functions. Three of the 
+#' functions---\linkInt{regTable}, \linkInt{latexTable}, and 
+#' \linkInt{latexTablePDF}---are more elaborate tools for making tables, and 
+#' they are designed to be used together. See the  
+#' \href{../doc/tables.html}{Building better tables in less time} vignette for
+#' an overview.
+#' 
+#' Of the convenience functions, I most often use \linkInt{qw}, 
+#' \linkInt{reliability}, \linkInt{meanNA}, and \linkInt{sumNA}.  
+
+#' @aliases Bullock
+#' @keywords internal
+"_PACKAGE" 
