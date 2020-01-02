dbd0e9d3b7af9c3c9200a9f45e5817b2223a5326
John Bullock <john@johnbullock.org>
2020-01-02 10:14:57 -0600

    Fixed checking for slashes in paths so that it works on Linux.

--- tests/testthat/test_latexTablePDF.R
+++ tests/testthat/test_latexTablePDF.R
@@ -7,8 +7,8 @@ context("Check that latexTablePDF() can write files.")
 
 # Check default behavior
 lT1 <- latexTable(matrix(1:16, nrow=4))
-lT1_filenameStem <- tempfile(tmpdir = '') 
-lT1_filenameStem <- gsub('^(\\\\*)', '', lT1_filenameStem)  # eliminate initial slashes
+lT1_filenameStem <- tempfile(tmpdir = '')  
+lT1_filenameStem <- gsub('^([/|\\\\]*)', '', lT1_filenameStem)  # eliminate initial slashes
 latexTablePDF(
   lT1, 
   outputFilenameStem = lT1_filenameStem,
@@ -24,7 +24,7 @@ latexTablePDF(
 # just one table.
 lT2 <- latexTable(matrix(1:16, nrow=4), commandName = 'secondTable')
 lT2_filenameStem <- tempfile(tmpdir = '')
-lT2_filenameStem <- gsub('^(\\\\*)', '', lT2_filenameStem)  # eliminate initial slashes
+lT2_filenameStem <- gsub('^([/|\\\\]*)', '', lT2_filenameStem)  # eliminate initial slashes
 latexTablePDF(
   list(lT1, lT2), 
   outputFilenameStem = lT2_filenameStem,
