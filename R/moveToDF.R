3291ecd8e736245daf7e15525ec654568dba9fc1
John Bullock <john@johnbullock.org>
2020-01-07 09:37:39 -0600

    Fixed bug affecting multiple modal lengths of objects in caller environ.

--- R/moveToDF.R
+++ R/moveToDF.R
@@ -6,7 +6,14 @@
 #' frame (or copied to a data frame if \code{move} is \code{FALSE}). 
 #' \code{pattern} may be \code{NULL}, in which case all variables in the 
 #' calling environment will be examined.
-#'   Variables that have the "dim" attribute -- for example, arrays and 
+#' 
+#' If there are multiple modal lengths of the objects in the calling 
+#' environment, all modes will be used. For example, if the calling 
+#' environment has 20 objects of length{\NB}1, 20 objects of length{\NB}2, and
+#' one object of length{\NB}3, the returned data frame may have as many as 40
+#' columns.
+#' 
+#' Variables that have the "dim" attribute -- for example, arrays and 
 #' matrices -- will not be moved into the new data frame. Functions will 
 #' never be moved into the new data frame, either.
 #' 
@@ -24,7 +31,7 @@ moveToDF <- function(pattern = NULL, move = TRUE) {
   # GET ALL VARIABLE NAMES
   if (!missing(pattern)) {
     stopifnot(is.character(pattern))
-    varNames <- ls(pat = pattern, envir = parent.frame())
+    varNames <- ls(pattern = pattern, envir = parent.frame())
   }
   else { 
     varNames <- ls(envir = parent.frame())
@@ -39,11 +46,17 @@ moveToDF <- function(pattern = NULL, move = TRUE) {
   # Remove, from varList, objects that don't have the modal length of all 
   # objects in the list.
   modalLength <- modalValue(lengths(varList))
+  if (length(modalLength) > 1)
+    message(paste0(
+      "There are multiple modal lengths for the objects in the calling environment: ",
+      paste(modalLength, collapse = ', '),
+      '.'))
   badClasses  <- 'function' 
   hasDim      <- sapply(varList, function (x) !is.null(dim(x)))  # matrices, arrays, etc.
   hasBadClass <- sapply(varList, function (x) any(class(x) %in% badClasses))
-  badVars     <- which(lengths(varList)!=modalLength | hasDim | hasBadClass)
-  varList     <- varList[-(badVars)]                             # remove objects from list
+  badVars     <- which(!lengths(varList)%in%modalLength | hasDim | hasBadClass)
+  if (length(badVars > 0)) 
+    varList   <- varList[-badVars]                               # remove objects from list
   
   
   # REMOVE OBJECTS FROM CALLING ENVIRONMENT IF move == TRUE
