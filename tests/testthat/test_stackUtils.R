8c6d4d65840906b2ccca8cb9b743da4ecd23dc08
John Bullock <john@johnbullock.org>
2020-01-07 10:41:17 -0600

    Added tests for the stack utilities: pop(), push(), stack(), unstack().

---
+++ tests/testthat/test_stackUtils.R
@@ -0,0 +1,36 @@
+# test_stackUtils.R
+# Created on 2019-01-07
+# Created by John Bullock
+
+context("Check output from the stack utilities: push(), pop(), shift(), and unshift()")
+
+test_that("push() and pop() work as expected", {
+  x <- new_stack()
+  push(x, 1:5)
+  y <- pop(x)
+  z <- pop(x)
+  expect_identical(x$.Data, 1:3)
+  expect_equal(y, 5)
+  expect_equal(z, 4)
+})
+
+test_that("shift() and unshift() work as expected", {
+  x <- new_stack()
+  shift(x, 1:5)
+  y <- unshift(x)
+  z <- unshift(x)
+  expect_identical(x$.Data, 3:5)
+  expect_equal(y, 1)
+  expect_equal(z, 2)
+})
+
+test_that("copies of a stack object don't inherit subsequent changes to that object", {
+  x <- new_stack(1:5)
+  xCopy <- x
+  push(x, 6)
+  expect_equal(x$.Data, 1:6)
+  expect_equal(xCopy$.Data, 1:5)
+})
+
+
+
