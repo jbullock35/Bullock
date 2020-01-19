b02793d434da0530a04f0e22438104cf4c2d840b
John Bullock <john@johnbullock.org>
2020-01-19 08:52:43 -0600

    Added test for "ivreg" estimates when using clustered SEs.

--- tests/testthat/test_regTable.R
+++ tests/testthat/test_regTable.R
@@ -21,7 +21,7 @@ test_that("regTable() checks appropriately for existence of clusterVar", {
 })
 
 
-test_that("regTable() produces correct output with IV regressions", {
+test_that("regTable() produces matrices of right class and dimensions with IV regressions", {
   if (!require(AER)) skip("AER package not available")
   iv1 <- ivreg(Sepal.Length ~ Petal.Length | Petal.Width,  data = iris)
   iv2 <- ivreg(Sepal.Length ~ Petal.Width  | Petal.Length, data = iris)
@@ -32,7 +32,7 @@ test_that("regTable() produces correct output with IV regressions", {
 })
 
 
-test_that("regTable() produces correct output with lm() objects and clustered SEs", {
+test_that("regTable() produces correct estimates and SEs with \"lm\" objects and clustering", {
   # We take this as the reference output:  [2020 01 19]
   #    
   # multiwayvcov::cluster.vcov(lm1, iris$Species)
@@ -54,3 +54,34 @@ test_that("regTable() produces correct output with lm() objects and clustered SE
   expect_equal(rT2_cluster[2, 4], sqrt(0.0124),  tol = 0.0005)
   expect_equal(rT2_cluster[3, 4], sqrt(0.0291),  tol = 0.0001)
 })
+
+
+
+test_that("regTable() produces correct estimates and SEs with \"ivreg\" objects and clustering", {
+  if (!require(AER)) skip("AER package not available")
+  if (!require(AER)) skip("ivpack package not available")
+  iv1 <- ivreg(Sepal.Length ~ Petal.Length | Petal.Width,  data = iris)
+  iv2 <- ivreg(Sepal.Length ~ Petal.Width  | Petal.Length, data = iris)
+  rT1_iv_cluster <- regTable(list(iv1),      clusterVar = list(iris$Species))
+  rT2_iv_cluster <- regTable(list(iv1, iv2), clusterVar = list(iris$Species))
+    
+  # We take this as the reference output:  [2020 01 19]
+  #    
+  # ivpack::cluster.robust.se(iv1, iris$Species)
+  #             Estimate Std. Error t value Pr(>|t|)    
+  # (Intercept)    4.3459     0.1360    32.0   <2e-16 ***
+  # Petal.Length   0.3985     0.0274    14.5   <2e-16 ***  
+  #
+  # ivpack::cluster.robust.se(iv2, iris$Species)
+  #           Estimate Std. Error t value Pr(>|t|)    
+  # (Intercept)   4.6637     0.1546    30.2   <2e-16 ***
+  # Petal.Width   0.9836     0.0862    11.4   <2e-16 ***    
+  expect_equal(rT1_iv_cluster[1, 2], 0.1360, tol = 0.0001)
+  expect_equal(rT1_iv_cluster[2, 2], 0.0274, tol = 0.0001)
+  
+  expect_equal(rT2_iv_cluster[1, 2], 0.1360, tol = 0.0001)
+  expect_equal(rT2_iv_cluster[2, 2], 0.0274, tol = 0.0001)
+  expect_equal(rT2_iv_cluster[1, 4], 0.1546,  tol = 0.0001)
+  expect_equal(rT2_iv_cluster[3, 4], 0.0862,  tol = 0.0005)
+})
+
