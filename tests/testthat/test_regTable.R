945db059cdf03f06dd570e4b8e4a5ba17c34014f
John Bullock <john@johnbullock.org>
2020-01-19 10:37:23 -0600

    Add test of regTable() calls that include lm & ivreg objs & cluster SEs.

--- tests/testthat/test_regTable.R
+++ tests/testthat/test_regTable.R
@@ -85,3 +85,23 @@ test_that("regTable() produces correct estimates and SEs with \"ivreg\" objects 
   expect_equal(rT2_iv_cluster[3, 4], 0.0862, tol = 0.0005)
 })
 
+
+test_that("regTable() produces correct estimates and SEs with both \"lm\" and \"ivreg\" objects and clustering", {
+  if (!require(AER)) skip("AER package not available")
+  if (!require(AER)) skip("ivpack package not available")
+  iv1 <- ivreg(Sepal.Length ~ Petal.Length | Petal.Width,  data = iris)
+  iv2 <- ivreg(Sepal.Length ~ Petal.Width  | Petal.Length, data = iris)
+  rT_mixed <- regTable(list(lm1, lm2, iv1, iv2), clusterVar = list(iris$Species))
+  
+  expect_equal(rT_mixed[1, 2], sqrt(0.03674), tol = 0.0001)
+  expect_equal(rT_mixed[2, 2], sqrt(0.00161), tol = 0.0001)
+  expect_equal(rT_mixed[1, 4], sqrt(0.0597),  tol = 0.0001)
+  expect_equal(rT_mixed[2, 4], sqrt(0.0124),  tol = 0.0005)
+  expect_equal(rT_mixed[3, 4], sqrt(0.0291),  tol = 0.0001)
+   
+  expect_equal(rT_mixed[1, 6], 0.1360, tol = 0.0001)
+  expect_equal(rT_mixed[2, 6], 0.0274, tol = 0.0001)
+  expect_equal(rT_mixed[1, 8], 0.1546, tol = 0.0001)
+  expect_equal(rT_mixed[3, 8], 0.0862, tol = 0.0005)
+
+})
